d98761b07c441b55cabecac4af12baa21a6ab8ac
Starbuck5 <46412508+Starbuck5@users.noreply.github.com>
2023-04-08 18:50:12 -0700

    Update freetype

--- src_c/freetype/ft_render.c
+++ src_c/freetype/ft_render.c
@@ -436,20 +436,12 @@ _PGFT_Render_NewSurface(FreeTypeInstance *ft, pgFontObject *fontobj,
     }
 
     if (bits_per_pixel == 32) {
-#if SDL_BYTEORDER == SDL_BIG_ENDIAN
-        rmask = 0xff000000;
-        gmask = 0x00ff0000;
-        bmask = 0x0000ff00;
-        amask = 0x000000ff;
-#else
-        rmask = 0x000000ff;
-        gmask = 0x0000ff00;
-        bmask = 0x00ff0000;
-        amask = 0xff000000;
-#endif
+        surface = PG_CreateSurface(width, height, SDL_PIXELFORMAT_BGRA32);
     }
-    surface = SDL_CreateRGBSurface(0, width, height, bits_per_pixel, rmask,
-                                   gmask, bmask, amask);
+    else {
+        surface = PG_CreateSurface(width, height, SDL_PIXELFORMAT_INDEX8);
+    }
+
     if (!surface) {
         PyErr_SetString(pgExc_SDLError, SDL_GetError());
         return 0;
